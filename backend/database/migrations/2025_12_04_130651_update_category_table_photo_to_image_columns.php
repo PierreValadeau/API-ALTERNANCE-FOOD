dd311c58dffb1a7967b8f1e43de8a0ec48e731f2
PierreValadeau <p.valadeau@outlook.fr>
2025-12-04 14:25:27 +0100

    feat: add migrations to replace photo with imageUrl and imagePath

---
+++ backend/database/migrations/2025_12_04_130651_update_category_table_photo_to_image_columns.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('category', function (Blueprint $table) {
+            $table->dropColumn('photo');
+            $table->string('imageUrl')->nullable();
+            $table->string('imagePath')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('category', function (Blueprint $table) {
+            $table->dropColumn(['imageUrl', 'imagePath']);
+            $table->string('photo')->nullable();
+        });
+    }
+};
